ce8b772cd57693585e7f0b410e0b81d1edab5a4b
Evan Shortiss <evanshortiss@gmail.com>
2023-11-03 09:31:24 -0700

    fix: database url code at build time

--- elements-application/next.config.js
+++ elements-application/next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
   // This is necessary to support deployment as a via container image
   output: 'standalone'
 }
